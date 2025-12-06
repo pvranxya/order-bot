b0e296617fd01e2a5ca909de29ba5cc599df3850
pvranxya <danielprist@mail.ru>
2025-12-06 13:15:57 +0300

    add menu and base

---
+++ order bot/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class MenuItem
+{
+    public int Id { get; set; }
+    public string Restaurant { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
